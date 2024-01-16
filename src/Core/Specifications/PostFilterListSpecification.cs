d6d1d440a7f64641cb8748683c5c3c5153a4356e
Umer <umertariq556@gmail.com>
2024-01-17 00:44:11 +0500

    added filter specification

---
+++ src/Core/Specifications/PostFilterListSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Specification;
+using Core.Entities.PostAggregate;
+
+namespace Core.Specifications
+{
+    public class PostFilterListSpecification : Specification<Post>
+    {
+        public PostFilterListSpecification(int skip, int take, string searchText = "")
+        {
+            if (skip < 0)
+            {
+                skip = 0;
+            }
+            if (take == 0)
+            {
+                take = int.MaxValue;
+            }
+
+            Query.Where(p => p.Title.ToLower().Contains(searchText.Trim().ToLower())
+            || p.Description.ToLower().Contains(searchText.Trim().ToLower())).Skip(skip).Take(take);
+        }
+    }
+}
